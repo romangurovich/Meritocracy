e0101b48e98e3b4f70e030a764fd60aa7bdae039
Roman Gurovich <romangurovich@gmail.com>
2013-04-11 09:11:36 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Meritocracy::Application.config.secret_token = 'f0bccdf8d39b0b4d0a3eac2061b1c093933e46b349537e644077db0169c0967522d32c03ecb149c34ae295842e0f50f80ce9bb8a3906afbc64460e9a7c2ef4d1'
