19d614fdbc6a8df58eff50ccad1894911b458e92
Roman Gurovich <romangurovich@gmail.com>
2013-04-11 16:45:13 -0700

    Implement team views

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,5 @@
 class Team < ActiveRecord::Base
-  attr_accessible :name, :supervisor_id
+  attr_accessible :name, :supervisor_id, :member_ids
 
   belongs_to :supervisor, class_name: "Employee", inverse_of: :supervised_teams
   has_many :memberships, class_name: "TeamMembership"
